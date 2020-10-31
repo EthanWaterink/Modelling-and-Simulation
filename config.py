3f49e0e78a409edc5f0401cabaa9542165f0d9ce
s3417611 <E.Waterink@student.rug.nl>
2020-10-31 12:05:23 +0100

    Update config

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ RANDOM_SEED = 42
 SIMULATIONS_PER_MODEL = 10
 
 # Grid
-GRID_WIDTH = 6
-GRID_HEIGHT = 5
+GRID_WIDTH = 10
+GRID_HEIGHT = 10
 ROAD_LENGTH = 1
 
 # Intersections
-NEIGHBOUR_PROBABILITY = 1  # The probability of an intersection being connected to a neighbouring one
+ROAD_PROBABILITY = .9  # The probability of an intersection being connected to a neighbouring one
+LANE_PROBABILITY = .9  # The probability of a lane at the end of a road
 
 # Vehicles
-VEHICLE_MIN_ROADS = 2
-VEHICLE_MAX_ROADS = 5
-MIN_VEHICLES = 3
-MAX_VEHICLES = 4
-DEFAULT_DIRECTION = 0
+VEHICLE_MIN_ROADS = 12
+VEHICLE_MAX_ROADS = 12
+MIN_VEHICLES = 2500
+MAX_VEHICLES = 2500
 
 # Run
-MAX_VEHICLES_PER_STEP = 4
+MAX_VEHICLES_PER_STEP = 5
